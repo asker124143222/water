798d71cae9be34ef7a7a9d480fd94ff6bbf158aa
asker124143222 <124143222@qq.com>
2020-03-30 17:32:14 +0800

    DruidConfig

---
+++ src/main/java/com/home/water/config/DruidConfig.java
@@ -0,0 +1,66 @@
+package com.home.water.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: xu.dm
+ * @Date: 2020/3/30 16:47
+ * @Version: 1.0
+ * @Description: TODO
+ **/
+@Configuration
+public class DruidConfig {
+
+    //  将yml里的配置参数注入DruidDataSource
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource dataSource(){
+        return new DruidDataSource();
+    }
+
+    //配置Druid监控
+    //1、配置管理后台的Servlet
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        Map<String, String> initParams = new HashMap<>();
+
+        initParams.put("loginUsername", "admin");
+        initParams.put("loginPassword", "123456");
+        //如果没有填就允许所有地址访问
+        initParams.put("allow", "localhost");
+        initParams.put("deny", "192.168.31.10");
+
+        bean.setInitParameters(initParams);
+        return bean;
+    }
+
+    // 2、配置一个web监控的filter
+    @Bean
+    public FilterRegistrationBean webStatFilter() {
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+
+        Map<String, String> initParams = new HashMap<>();
+        initParams.put("exclusions", "*.js,*.css,/druid/*");
+
+        bean.setInitParameters(initParams);
+
+        bean.setUrlPatterns(Arrays.asList("/*"));
+
+        return bean;
+    }
+
+}
